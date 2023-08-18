464114cd78dd4ce9642f45ac6ad4655267584164
Howl404 <114860794+Howl404@users.noreply.github.com>
2023-08-18 23:40:24 +0300

    feat: add aliases

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './App.scss';
-import Header from './components/Header/Header';
-import Home from './pages/Home/Home';
-import LoginPage from './pages/Login/LoginPage';
-import NotFound from './pages/NotFound/NotFound';
-import RegistrationPage from './pages/Register/RegistrationPage';
+import Header from '@components/Header/Header';
+import Home from '@pages/Home/Home';
+import LoginPage from '@pages/Login/LoginPage';
+import NotFound from '@pages/NotFound/NotFound';
+import RegistrationPage from '@pages/Register/RegistrationPage';
 
 function App(): JSX.Element {
   return (
