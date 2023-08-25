3e51fda828869b6e0d3be9bc88290aad120b9254
Michail <36785100+AcademeG1@users.noreply.github.com>
2023-08-25 13:33:51 +0300

    feat: add test navigates to /login and /register on buttons clicks for Home component

--- src/pages/Home/__tests__/Home.test.tsx
+++ src/pages/Home/__tests__/Home.test.tsx
@@ -15,4 +15,22 @@ describe('Home Component', () => {
     expect(container.querySelector('.main-heading')).toBeInTheDocument();
     expect(container.querySelector('.wrapper-btn')).toBeInTheDocument();
   });
+
+  it('navigates to /login and /register on button clicks', () => {
+    const { getByText } = render(
+      <BrowserRouter>
+        <Home />
+      </BrowserRouter>,
+    );
+
+    const loginButton = getByText('Login');
+    expect(loginButton).toBeInTheDocument();
+    fireEvent.click(loginButton);
+    expect(document.location.href).toContain('/login');
+
+    const registerButton = getByText('Register');
+    expect(registerButton).toBeInTheDocument();
+    fireEvent.click(registerButton);
+    expect(document.location.href).toContain('/register');
+  });
 });
