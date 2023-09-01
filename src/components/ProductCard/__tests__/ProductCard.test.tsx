2c7b885ab372c59a5076be77d3a96785cde4caa3
Howl404 <114860794+Howl404@users.noreply.github.com>
2023-09-01 14:33:20 +0300

    feat: product card test

---
+++ src/components/ProductCard/__tests__/ProductCard.test.tsx
@@ -0,0 +1,105 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import '@testing-library/jest-dom';
+import { Product } from '@src/interfaces/Product';
+import ProductCard from '../ProductCard';
+
+const mockProductWithDiscount: Product = {
+  name: { en: 'Product Name' },
+  description: { en: 'Product Description' },
+  masterVariant: {
+    id: 123,
+    sku: 'test',
+    images: [{ url: 'image_url' }],
+    prices: [
+      {
+        id: 'price_id',
+        value: {
+          currencyCode: 'USD',
+          centAmount: 1000,
+          fractionDigits: 2,
+        },
+        discounted: {
+          value: {
+            centAmount: 500,
+          },
+        },
+      },
+    ],
+    attributes: [
+      {
+        name: 'brand',
+        value: 'test',
+      },
+    ],
+  },
+  categories: [
+    {
+      typeId: 'test',
+      id: 'test',
+    },
+  ],
+};
+
+const mockProductWithoutDiscount: Product = {
+  name: { en: 'Product Name' },
+  description: { en: 'Product Description' },
+  masterVariant: {
+    id: 123,
+    sku: 'test',
+    images: [{ url: 'image_url' }],
+    prices: [
+      {
+        id: 'price_id',
+        value: {
+          currencyCode: 'USD',
+          centAmount: 1000,
+          fractionDigits: 2,
+        },
+      },
+    ],
+    attributes: [
+      {
+        name: 'brand',
+        value: 'test',
+      },
+    ],
+  },
+  categories: [
+    {
+      typeId: 'test',
+      id: 'test',
+    },
+  ],
+};
+
+describe('ProductCard Component', () => {
+  it('renders product information correctly', () => {
+    const { getByText, getByAltText } = render(<ProductCard product={mockProductWithDiscount} />);
+
+    expect(getByText('Product Name')).toBeInTheDocument();
+    expect(getByText('Product Description...')).toBeInTheDocument();
+    expect(getByText('10 USD')).toBeInTheDocument();
+    expect(getByText('5 USD')).toBeInTheDocument();
+
+    const productImage = getByAltText('Product Name');
+    expect(productImage).toBeInTheDocument();
+    expect(productImage).toHaveAttribute('src', 'image_url');
+  });
+});
+
+describe('ProductCard Component', () => {
+  it('renders product card with discounted price when discounted price exists', () => {
+    const { getByText } = render(<ProductCard product={mockProductWithDiscount} />);
+
+    expect(getByText('5 USD')).toBeInTheDocument();
+    expect(getByText('10 USD')).toBeInTheDocument();
+  });
+
+  it('renders product card with regular price when discounted price does not exist', () => {
+    const { getByText } = render(<ProductCard product={mockProductWithoutDiscount} />);
+
+    expect(getByText('10 USD')).toBeInTheDocument();
+  });
+});
