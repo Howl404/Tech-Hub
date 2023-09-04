4d43ee33032626eeaec99f2bf8ce2d7612d5999c
Howl404 <114860794+Howl404@users.noreply.github.com>
2023-09-05 01:18:00 +0300

    fix: return product catalog

--- src/interfaces/Product.ts
+++ src/interfaces/Product.ts
@@ -1,3 +1,49 @@
+export interface ProductCatalog {
+  description: {
+    en: string;
+  };
+  name: {
+    en: string;
+  };
+  key: string;
+  masterVariant: {
+    id: number;
+    sku: string;
+    images: [
+      {
+        url: string;
+      },
+    ];
+    prices: [
+      {
+        id: string;
+        value: {
+          currencyCode: string;
+          centAmount: number;
+          fractionDigits: number;
+        };
+        discounted?: {
+          value: {
+            centAmount: number;
+          };
+        };
+      },
+    ];
+    attributes: [
+      {
+        name: string;
+        value: string;
+      },
+    ];
+  };
+  categories: [
+    {
+      typeId: string;
+      id: string;
+    },
+  ];
+}
+
 export interface ProductDetailedPage {
   id: string;
   version: number;
