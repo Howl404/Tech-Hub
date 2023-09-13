5a8d6fa01f860e74e8ee09653ef71aa9c5c3c30c
Howl404 <114860794+Howl404@users.noreply.github.com>
2023-09-13 17:39:28 +0300

    feat: collaboration text, styles

--- src/pages/About/AboutPage.tsx
+++ src/pages/About/AboutPage.tsx
@@ -43,6 +43,20 @@ export default function AboutPage(): JSX.Element {
           <AboutCard member={member} key={member.name} />
         ))}
       </div>
+      <div className="about__collaboration-wrapper">
+        <p className="item">
+          We actively conducted code reviews among team members using a combination of tools like Jira and GitHub. This
+          ensured that each line of code met our high standards for quality and efficiency. Our teamwork extended beyond
+          just coding, we held regular discussions to brainstorm ideas, assist each other with challenges, and refine
+          our project&#39;s direction.
+        </p>
+        <p className="item">
+          This collaborative process not only improved the overall codebase but also fostered a culture of knowledge
+          sharing and continuous improvement. We maintained a disciplined approach with regular commits and a clear pull
+          request system, which contributed to the project&#39;s success and our ability to stay organized and on track.
+        </p>
+      </div>
+
       <div className="about__rs-school">
         <Link className="rs-school_link" to="https://rs.school">
           <img className="rs-school_logo" src="https://rs.school/images/rs_school_js.svg" alt="RS School" />
